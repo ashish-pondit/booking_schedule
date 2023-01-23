ee84af969fe6bb3b5fdaad77d8dce0b6e6927bb5
Ashish Pondit <ashish.pondit@dsinnovators.com>
2023-01-23 11:27:05 +0600

    test models generated

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class DliiliUser(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(null=True)
+    user_type = models.CharField(max_length=10, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Guide(DliiliUser):
+    price = models.PositiveIntegerField(default=50, null=True)
+
+
+class Tourist(DliiliUser):
+    country = models.CharField(max_length=100, null=True)
